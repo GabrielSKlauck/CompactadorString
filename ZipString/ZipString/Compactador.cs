195f9cdd39e27b87ba42bea8d1ea3e1b5c1834ae
GabrielSKlauck <112292289+GabrielSKlauck@users.noreply.github.com>
2024-03-20 19:43:34 -0300

    Inicio

---
+++ ZipString/ZipString/Compactador.cs
@@ -0,0 +1,40 @@
+﻿
+namespace ZipString
+{
+    public class Compactador
+    {
+        public string Compactar(string base64)
+        {
+            string base64Compactada;
+            for (int i = 0; i < base64.Length; i++)
+            {
+                int contador = 1;
+                string encurtador;
+                try
+                {
+                    while (base64[i] == base64[i + contador])
+                    {
+                        contador++;
+                    }
+                    if (contador != 1)
+                    {
+                        base64 = base64.Remove(i + 1, contador - 1);
+                        encurtador = base64[i] + "-" + (contador - 1);
+                        base64 = base64.Remove(i, i);
+                        base64 = base64.Insert(i, encurtador);
+                    }
+
+
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                contador = 1;
+            }
+            Console.WriteLine(base64);
+            return "";
+        }
+    }
+}
---
+++ ZipString/ZipString/Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipString
+{
+    public class Principal
+    {
+        public static void Main(string[] args)
+        {
+            Compactador comp = new Compactador();
+            comp.Compactar("dddaaaaehfffnggtttjaaaa");
+        }
+    }
+}
